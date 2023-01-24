0e88b6c957606409c7bb92b7d241f4819932e43c
Levi <47727379+lbare@users.noreply.github.com>
2023-01-24 14:59:23 -0800

    Initial setup

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import * as eva from '@eva-design/eva';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { Button } from '@ui-kitten/components';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const HomeScreen = () => (
+  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text category='h1'>Test</Text>
+  </Layout>
+);
+
+export default () => (
+  <ApplicationProvider {...eva} theme={eva.light}>
+    <HomeScreen />
+  </ApplicationProvider>
+);
---
+++ config/firebaseConfig.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Optionally import the services that you want to use
+// import {...} from "firebase/auth";
+// import {...} from "firebase/database";
+// import {...} from "firebase/firestore";
+// import {...} from "firebase/functions";
+// import {...} from "firebase/storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "***REMOVED***",
+  authDomain: "***REMOVED***",
+  projectId: "***REMOVED***",
+  storageBucket: "***REMOVED***.appspot.com",
+  messagingSenderId: "***REMOVED***",
+  appId: "1:***REMOVED***:web:c3c263d8e4907f92a621f6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
