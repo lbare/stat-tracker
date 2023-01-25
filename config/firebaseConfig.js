9195a6d689cd8ec6362949d39e51ea7ce2d3d135
Levi <47727379+lbare@users.noreply.github.com>
2023-01-24 18:25:06 -0800

    start Firebase authentication

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 // Optionally import the services that you want to use
-// import {...} from "firebase/auth";
 // import {...} from "firebase/database";
 // import {...} from "firebase/firestore";
 // import {...} from "firebase/functions";
@@ -10,13 +10,16 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBOgTaYS8HJA1uFEebcKBFukJ72Je_j5GM",
-  authDomain: "stat-tracker-9f25a.firebaseapp.com",
-  projectId: "stat-tracker-9f25a",
-  storageBucket: "stat-tracker-9f25a.appspot.com",
-  messagingSenderId: "491322406723",
-  appId: "1:491322406723:web:c3c263d8e4907f92a621f6"
+  apiKey: 'AIzaSyBOgTaYS8HJA1uFEebcKBFukJ72Je_j5GM',
+  authDomain: 'stat-tracker-9f25a.firebaseapp.com',
+  projectId: 'stat-tracker-9f25a',
+  storageBucket: 'stat-tracker-9f25a.appspot.com',
+  messagingSenderId: '491322406723',
+  appId: '1:491322406723:web:c3c263d8e4907f92a621f6',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
