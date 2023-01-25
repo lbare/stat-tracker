bbc78a2ae06e6761c6bd1341cac12b9399fc939d
Levi <47727379+lbare@users.noreply.github.com>
2023-01-25 13:10:34 -0800

    Add custom font

--- App.js
+++ App.js
@@ -1,9 +1,66 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  ActivityIndicator,
+} from 'react-native';
 import Auth from './scenes/Auth';
+import { useFonts } from 'expo-font';
 
-export default () => (
-  <View>
-    <Text>TEST</Text>
-  </View>
-);
+export default () => {
+  const [fontsLoaded] = useFonts({
+    'SecularOne-Regular': require('./assets/fonts/SecularOne-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <ActivityIndicator />
+      </View>
+    );
+  }
+  return (
+    <View>
+      <Auth styles={styles} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    alignContent: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
+    backgroundColor: '#2D424F',
+  },
+  topBar: {
+    width: Dimensions.get('window').width,
+    justifyContent: 'center',
+    paddingTop: 100,
+    paddingBottom: 10,
+    backgroundColor: '#FFF8DE',
+  },
+  middle: {
+    padding: 20,
+  },
+  bottom: {
+    padding: 20,
+  },
+  h1: {
+    fontFamily: 'SecularOne-Regular',
+    textAlign: 'center',
+    fontSize: 48,
+    color: '#2D424F',
+  },
+  p: {
+    fontFamily: 'SecularOne-Regular',
+    textAlign: 'center',
+    fontSize: 24,
+    color: '#FFF8DE',
+  },
+});
