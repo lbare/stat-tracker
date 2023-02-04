5eb8b19a10c1915d7db9e232331c0d5046b2e905
Levi <47727379+lbare@users.noreply.github.com>
2023-02-04 15:52:01 -0800

    Edit firebase config

--- firebase.js
+++ firebase.js
@@ -1,18 +1,46 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { getApps, initializeApp, getApp } from 'firebase/app';
+import {
+  initializeAuth,
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  docRef,
+  doc,
+  getDoc,
+  setDoc
+} from 'firebase/firestore';
+import { getReactNativePersistence } from 'firebase/auth/react-native';
+import Asyncstorage from '@react-native-async-storage/async-storage';
+import { useEffect } from 'react';
+import { async } from '@firebase/util';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBOgTaYS8HJA1uFEebcKBFukJ72Je_j5GM",
-  authDomain: "stat-tracker-9f25a.firebaseapp.com",
-  projectId: "stat-tracker-9f25a",
-  storageBucket: "stat-tracker-9f25a.appspot.com",
-  messagingSenderId: "491322406723",
-  appId: "1:491322406723:web:c3c263d8e4907f92a621f6",
+  apiKey: 'AIzaSyBOgTaYS8HJA1uFEebcKBFukJ72Je_j5GM',
+  authDomain: 'stat-tracker-9f25a.firebaseapp.com',
+  projectId: 'stat-tracker-9f25a',
+  storageBucket: 'stat-tracker-9f25a.appspot.com',
+  messagingSenderId: '491322406723',
+  appId: '1:491322406723:web:c3c263d8e4907f92a621f6',
 };
 
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { db, auth, collection, addDoc, createUserWithEmailAndPassword };
+export {
+  db,
+  auth,
+  collection,
+  addDoc,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  docRef,
+  doc,
+  getDoc,
+  setDoc
+};
