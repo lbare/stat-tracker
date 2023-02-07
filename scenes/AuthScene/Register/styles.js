9626ae1404f0a8caf68b506e6f98281d8e056e23
Levi <47727379+lbare@users.noreply.github.com>
2023-02-07 11:27:42 -0800

    Add gradient to auth scene, adjust styling

--- scenes/AuthScene/Register/styles.js
+++ scenes/AuthScene/Register/styles.js
@@ -8,23 +8,17 @@ export const styles = StyleSheet.create({
   },
   main: {
     borderTopLeftRadius: 50,
-    backgroundColor: colors.blue[900],
     flex: 1,
     paddingVertical: spacing.m,
   },
   middle1: {
     paddingTop: spacing.s,
-    flex: 0.53,
+    flex: 0.5,
     paddingVertical: spacing.s,
   },
   middle2: {
     flex: 0.4,
     paddingVertical: spacing.s,
-  },
-  bottom: {
-    flex: 0.12,
-    backgroundColor: colors.blue[900],
-    justifyContent: 'flex-start',
   },
   // Text
   h1: {
@@ -34,6 +28,7 @@ export const styles = StyleSheet.create({
   },
   p: {
     ...font.body,
+    paddingTop: spacing.m,
     fontSize: fontSize.s,
     textAlign: 'center',
     color: 'white',
