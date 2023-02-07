d377741e3b7e2331f82519ea2720dd527399135c
Levi <47727379+lbare@users.noreply.github.com>
2023-02-06 18:42:44 -0800

    Add section list for main screen

--- scenes/HomeScene/LogGame/index.js
+++ scenes/HomeScene/LogGame/index.js
@@ -1,12 +1,16 @@
-import React from "react";
-import { View, SafeAreaView, Text } from "react-native";
+import React from 'react';
+import { View, SafeAreaView, Text, StatusBar } from 'react-native';
+import { List } from 'phosphor-react-native';
+import { styles } from '../Stats/styles';
 
 export const LogGame = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>TEST</Text>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle='dark-content' />
+      <View style={styles.top}>
+        <Text style={styles.h1}>TEST</Text>
       </View>
+      <View style={styles.middle}></View>
     </SafeAreaView>
   );
 };
