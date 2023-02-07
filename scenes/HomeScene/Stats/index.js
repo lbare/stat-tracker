b0b544effa01f52fe1b889b892271e99085be17c
Levi <47727379+lbare@users.noreply.github.com>
2023-02-06 18:42:26 -0800

    Reorganize screens

--- scenes/HomeScene/Stats/index.js
+++ scenes/HomeScene/Stats/index.js
@@ -1,12 +1,28 @@
-import React from "react";
-import { View, SafeAreaView, Text } from "react-native";
+import { View, SafeAreaView, StatusBar } from "react-native";
+import { styles } from "./styles";
+import { colors } from "../../../styles";
+import {
+  UserCircle,
+  House,
+  Gear,
+  ChartLine,
+  PlusCircle,
+} from "phosphor-react-native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { Text } from "react-native-paper";
+import React, { useState } from "react";
 
-export const Stats = () => {
+export const Home = (props) => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>TEST</Text>
+    <SafeAreaView style={styles.container}>
+      {/* Top Bar */}
+      <StatusBar barStyle='dark-content' />
+      <View style={styles.top}>
+        <Text style={styles.h1}>StatTracker</Text>
+        <UserCircle size={40} color={colors.blue[900]} />
       </View>
+      {/* Main */}
+      <View style={styles.middle}></View>
     </SafeAreaView>
   );
 };
--- scenes/HomeScene/Stats/styles.js
+++ scenes/HomeScene/Stats/styles.js
@@ -1,1 +1,24 @@
+import { StyleSheet } from "react-native";
+import { fontSize, font, spacing, colors } from "../../../styles";
 
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  top: {
+    flex: 0.1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: spacing.m,
+  },
+  middle: {
+    flex: 1,
+    backgroundColor: colors.blue[900],
+  },
+  h1: {
+    ...font.h2,
+    color: colors.blue[900],
+  },
+});
