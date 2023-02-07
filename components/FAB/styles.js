5bfabdc9534fdf897ded38acc6f13e7ec7ad3059
Levi <47727379+lbare@users.noreply.github.com>
2023-02-07 12:02:09 -0800

    Add floating button component

---
+++ components/FAB/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  fab: {
+    position: 'absolute',
+    backgroundColor: 'orange',
+    right: 20,
+    bottom: 30,
+  },
+});
