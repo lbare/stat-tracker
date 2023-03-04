6cdfe7c32cfbe2c66650f7ec48000bdc548f7f16
lbare <levi.bare@gmail.com>
2023-03-04 14:11:36 -0800

    readd firebase configs from .env

--- firebase.js
+++ firebase.js
@@ -19,12 +19,12 @@ import { getReactNativePersistence } from "firebase/auth/react-native";
 import { useEffect } from "react";
 
 const firebaseConfig = {
-  
-  
-  
-  
-  
-  
+  apiKey: Constants.manifest.extra.firebaseApiKey,
+  authDomain: Constants.manifest.extra.firebaseAuthDomain,
+  projectId: Constants.manifest.extra.firebaseProjectId,
+  storageBucket: Constants.manifest.extra.firebaseStorageBucket,
+  messagingSenderId: Constants.manifest.extra.firebaseMessagingSenderId,
+  appId: Constants.manifest.extra.firebaseAppId,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
