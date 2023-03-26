11b69e838f490bc6dc3bc5b3d296d3595cb4d533
Levi <47727379+lbare@users.noreply.github.com>
2023-03-26 12:21:23 -0700

    add stepindicator mock

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,30 +1,25 @@
-import React, { useRef } from 'react';
-import { View, Text, TouchableOpacity, TextInput } from 'react-native';
-import { auth, signInWithEmailAndPassword, db, doc, getDoc } from '../firebase';
+import React from "react";
+import { View, Text, TouchableOpacity, TextInput } from "react-native";
+import { auth, signInWithEmailAndPassword, db, doc, getDoc } from "../firebase";
 
 const Login = ({ navigation }) => {
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState({
-    password: '',
-    showPassword: false,
-  });
-
-  const ref_to_input2 = useRef();
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
   const onSignupPress = () => {
-    navigation.navigate('Register');
+    navigation.navigate("Register");
   };
 
   const onLoginPress = () => {
-    signInWithEmailAndPassword(auth, email, password.password)
+    signInWithEmailAndPassword(auth, email, password)
       .then((userCredentials) => {
         const uid = userCredentials.user.uid;
-        getDoc(doc(db, 'users', uid)).then((docSnap) => {
+        getDoc(doc(db, "users", uid)).then((docSnap) => {
           if (docSnap.exists()) {
             const user = docSnap.data();
-            navigation.navigate('HomeScene', { user: user });
+            navigation.navigate("HomeScene", { user: user });
           } else {
-            console.log('No such document');
+            console.log("No such document");
           }
         });
       })
@@ -34,54 +29,49 @@ const Login = ({ navigation }) => {
   };
 
   return (
-    <View className='flex-1 justify-center bg-white'>
-      <View className='h-1/4'>
-        <Text className='text-7xl pb-20 font-black text-center text-blue-500 pt-1'>
+    <View className="flex-1 justify-center bg-white">
+      <View className="h-1/4">
+        <Text className="text-7xl pb-20 font-black text-center text-blue-500 pt-1">
           Sign In
         </Text>
       </View>
-      <View className='h-1/4'>
+      <View className="h-1/4">
         <TextInput
-          className='bg-gray-100 border border-gray-500 mx-10 p-4 rounded-lg'
-          title='Email'
-          placeholder='Email'
+          className="bg-gray-100 border border-gray-500 mx-10 p-4 rounded-lg"
+          title="Email"
+          placeholder="Email"
           blurOnSubmit={false}
           secureTextEntry={false}
-          keyboardType='email-address'
-          textContentType='emailAddress'
+          keyboardType="email-address"
+          textContentType="emailAddress"
           value={email}
-          onChangeText={(value) => setEmail(value)}
-          returnKeyType='next'
-          onSubmitEditing={() => ref_to_input2.current.focus()}
+          onChangeText={(value) => setEmail(value.toString())}
+          returnKeyType="next"
         />
         <TextInput
-          className='bg-gray-100 border border-gray-500 mx-10 mt-5 p-4 rounded-lg'
-          title='Password'
-          placeholder='Password'
+          className="bg-gray-100 border border-gray-500 mx-10 mt-5 p-4 rounded-lg"
+          title="Password"
+          placeholder="Password"
           blurOnSubmit={true}
-          secureTextEntry={!password.showPassword}
-          keyboardType='default'
-          textContentType='password'
+          keyboardType="default"
+          textContentType="password"
           value={password}
-          onChangeText={(value) =>
-            setPassword({ ...password, password: value })
-          }
-          returnKeyType='done'
-          refInner={ref_to_input2}
+          onChangeText={(value) => setPassword(value.toString())}
+          returnKeyType="done"
           onSubmitEditing={onLoginPress}
         />
         <TouchableOpacity
-          className='bg-blue-500 mx-10 mt-5 py-5 rounded-lg'
+          className="bg-blue-500 mx-10 mt-5 py-5 rounded-lg"
           onPress={onLoginPress}
         >
-          <Text className='text-xl text-center text-white'>Login</Text>
+          <Text className="text-xl text-center text-white">Login</Text>
         </TouchableOpacity>
         <Text
-          className='mt-5 text-center text-20 text-blue-900 text-sm'
+          className="mt-5 text-center text-20 text-blue-900 text-sm"
           onPress={onSignupPress}
           suppressHighlighting={true}
         >
-          Don't have an account?{'\n'}SIGN UP
+          Don't have an account?{"\n"}SIGN UP
         </Text>
       </View>
     </View>
--- __tests__/Screens/LogGameScreen.test.js
+++ __tests__/Screens/LogGameScreen.test.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
+import React from "react";
+import renderer from "react-test-renderer";
+import LogGameScreen from "../../Screens/LogGameScreen";
 
-import LogGameScreen from '../../Screens/LogGameScreen';
+jest.mock("react-native-step-indicator");
 
-describe('<LogGameScreen />', () => {
-  it('renders correctly', () => {
+describe("<LogGameScreen />", () => {
+  it("renders correctly", () => {
     const tree = renderer.create(<LogGameScreen />).toJSON();
     expect(tree).toMatchSnapshot();
   });
