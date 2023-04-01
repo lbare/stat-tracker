a3c64d8585f695877bbf01f7a91d4b5e9f85287a
Levi <47727379+lbare@users.noreply.github.com>
2023-04-01 16:25:07 -0700

    utilize new firebase functions

--- Screens/RegisterScreen.js
+++ Screens/RegisterScreen.js
@@ -1,110 +1,85 @@
-import React, { useRef } from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
-import {
-  setDoc,
-  auth,
-  db,
-  doc,
-  createUserWithEmailAndPassword,
-} from '../firebase';
+import React, { useContext, useEffect } from "react";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { register } from "../services/firebase";
+import { AuthContext } from "../components/AuthContext";
 
 const Register = ({ navigation }) => {
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [confirmPassword, setConfirmPassword] = React.useState('');
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [confirmPassword, setConfirmPassword] = React.useState("");
 
-  const ref_to_input2 = useRef();
-  const ref_to_input3 = useRef();
+  const user = useContext(AuthContext);
 
-  const onLoginPress = () => {
-    navigation.navigate('Login');
-  };
-
+  const onLoginPress = () => navigation.navigate("Login");
   const onRegisterPress = () => {
     if (password !== confirmPassword) {
       alert("Passwords don't match.");
       return;
     }
-
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((response) => {
-        const uid = response.user.uid;
-        const data = {
-          uid: uid,
-          email,
-        };
-        setDoc(doc(db, 'users', uid), data)
-          .then(navigation.navigate('HomeScene', { user: data }))
-          .catch((error) => {
-            alert(error);
-          });
-      })
-      .catch((error) => {
-        alert(error);
-      });
+    register(email, password);
   };
 
+  useEffect(() => {
+    if (user) navigation.navigate("Home");
+  }, []);
+
   return (
-    <View className='flex-1 justify-center bg-white'>
-      <View className='h-1/4'>
-        <Text className='text-7xl pb-20 font-black text-center text-blue-500 pt-1'>
+    <View className="flex-1 justify-center bg-white">
+      <View className="h-1/4">
+        <Text className="text-7xl pb-20 font-black text-center text-blue-500 pt-1">
           Sign Up
         </Text>
       </View>
-      <View className='h-1/4'>
+      <View className="h-1/4">
         <TextInput
-          className='bg-gray-100 border border-gray-500 mx-10 p-4 rounded-lg'
-          title='Email'
-          placeholder='Email'
+          className="bg-gray-100 border border-gray-500 mx-10 p-4 rounded-lg"
+          title="Email"
+          placeholder="Email"
           blurOnSubmit={false}
           secureTextEntry={false}
-          keyboardType='email-address'
-          textContentType='emailAddress'
+          keyboardType="email-address"
+          textContentType="emailAddress"
           value={email}
           onChangeText={(value) => setEmail(value)}
-          returnKeyType='next'
-          onSubmitEditing={() => ref_to_input2.current.focus()}
+          returnKeyType="next"
         />
         <TextInput
-          className='bg-gray-100 border border-gray-500 mx-10 mt-5 p-4 rounded-lg'
-          title='Password'
-          placeholder='Password'
+          className="bg-gray-100 border border-gray-500 mx-10 mt-5 p-4 rounded-lg"
+          title="Password"
+          placeholder="Password"
           blurOnSubmit={false}
           secureTextEntry={true}
-          keyboardType='default'
-          textContentType='password'
+          keyboardType="default"
+          textContentType="password"
           value={password}
           onChangeText={(value) => setPassword(value)}
-          returnKeyType='next'
-          refInner={ref_to_input2}
-          onSubmitEditing={() => ref_to_input3.current.focus()}
+          returnKeyType="next"
         />
         <TextInput
-          className='bg-gray-100 border border-gray-500 mx-10 mt-5 p-4 rounded-lg'
-          title='Confirm Password'
-          placeholder='Confirm Password'
+          className="bg-gray-100 border border-gray-500 mx-10 mt-5 p-4 rounded-lg"
+          title="Confirm Password"
+          placeholder="Confirm Password"
           blurOnSubmit={true}
           secureTextEntry={true}
-          keyboardType='default'
-          textContentType='password'
+          keyboardType="default"
+          textContentType="password"
           value={confirmPassword}
           onChangeText={(value) => setConfirmPassword(value)}
-          returnKeyType='done'
-          refInner={ref_to_input3}
+          returnKeyType="done"
           onSubmitEditing={onRegisterPress}
         />
         <TouchableOpacity
-          className='bg-blue-500 mx-10 mt-5 py-5 rounded-lg'
+          className="bg-blue-500 mx-10 mt-5 py-5 rounded-lg"
           onPress={onRegisterPress}
         >
-          <Text className='text-xl text-center text-white'>Create Account</Text>
+          <Text className="text-xl text-center text-white">Create Account</Text>
         </TouchableOpacity>
         <Text
-          className='mt-5 text-center text-20 text-blue-900 text-sm'
+          className="mt-5 text-center text-20 text-blue-900 text-sm"
           onPress={onLoginPress}
           suppressHighlighting={true}
         >
-          Already have an account?{'\n'}SIGN IN
+          Already have an account?{"\n"}SIGN IN
         </Text>
       </View>
     </View>
