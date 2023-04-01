3820d1f5340b57b8a9c0625fc1d97d0a83960e00
Levi <47727379+lbare@users.noreply.github.com>
2023-03-31 17:00:33 -0700

    add SVGs

---
+++ assets/SVG/Medium.js
@@ -0,0 +1,24 @@
+import * as React from "react";
+import Svg, { Path } from "react-native-svg";
+import { View } from "react-native";
+
+const Medium = ({ options }) => (
+  <Svg
+    id="Medium"
+    data-name="Medium"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 556.44 273.88"
+    style={{
+      enableBackground: "new 0 0 612 792",
+    }}
+    xmlSpace="preserve"
+    height="500"
+    width="500"
+  >
+    <Path className="cls-1" d="M2.5 271.38s275.72-604.97 551.44 0" />
+    <Path className="cls-1" d="M2.5 271.38s275.72-362.63 551.44 0" />
+    <Path className="cls-1" d="M2.5 271.38s275.72-186.51 551.44 0" />
+    <Path className="cls-1" d="M2.5 271.38s275.72-41.45 551.44 0" />
+  </Svg>
+);
+export default Medium;
