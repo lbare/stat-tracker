a1865cf2de04106114f20b424e7d6bd8930a0e05
lbare <levi.bare@gmail.com>
2023-04-03 13:24:55 -0700

    add keyboard avoiding view for inputs

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,5 +1,12 @@
 import React, { useContext, useEffect } from "react";
-import { View, Text, TouchableOpacity, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  Keyboard,
+  KeyboardAvoidingView,
+} from "react-native";
 import { login } from "../services/firebase";
 import { AuthContext } from "../components/AuthContext";
 
@@ -20,52 +27,56 @@ const Login = ({ navigation }) => {
   }, []);
 
   return (
-    <View className="flex-1 justify-center bg-white">
-      <View className="h-1/4">
-        <Text className="text-7xl pb-20 font-black text-center text-blue-500 pt-1">
+    <KeyboardAvoidingView
+      className='flex-1 justify-center bg-white'
+      behavior='padding'
+    >
+      <View className='h-1/4'>
+        <Text className='text-7xl pb-20 font-black text-center text-blue-500 pt-1'>
           Sign In
         </Text>
       </View>
-      <View className="h-1/4">
+      <View className='h-1/4'>
         <TextInput
-          className="bg-gray-100 border border-gray-500 mx-10 p-4 rounded-lg"
-          title="Email"
-          placeholder="Email"
+          className='bg-gray-100 border border-gray-500 mx-10 p-4 rounded-lg'
+          title='Email'
+          placeholder='Email'
           blurOnSubmit={false}
           secureTextEntry={false}
-          keyboardType="email-address"
-          textContentType="emailAddress"
+          keyboardType='email-address'
+          textContentType='emailAddress'
           value={email}
           onChangeText={(value) => setEmail(value.toString())}
-          returnKeyType="next"
+          onSubmitEditing={() => Keyboard.dismiss()}
+          returnKeyType='next'
         />
         <TextInput
-          className="bg-gray-100 border border-gray-500 mx-10 mt-5 p-4 rounded-lg"
-          title="Password"
-          placeholder="Password"
+          className='bg-gray-100 border border-gray-500 mx-10 mt-5 p-4 rounded-lg'
+          title='Password'
+          placeholder='Password'
           blurOnSubmit={true}
-          keyboardType="default"
-          textContentType="password"
+          keyboardType='default'
+          textContentType='password'
           value={password}
           onChangeText={(value) => setPassword(value.toString())}
-          returnKeyType="done"
+          returnKeyType='done'
           onSubmitEditing={onLoginPress}
         />
         <TouchableOpacity
-          className="bg-blue-500 mx-10 mt-5 py-5 rounded-lg"
+          className='bg-blue-500 mx-10 mt-5 py-5 rounded-lg'
           onPress={onLoginPress}
         >
-          <Text className="text-xl text-center text-white">Login</Text>
+          <Text className='text-xl text-center text-white'>Login</Text>
         </TouchableOpacity>
         <Text
-          className="mt-5 text-center text-20 text-blue-900 text-sm"
+          className='mt-5 text-center text-20 text-blue-900 text-sm'
           onPress={onRegisterPress}
           suppressHighlighting={true}
         >
           Don't have an account?{"\n"}SIGN UP
         </Text>
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
--- Screens/RegisterScreen.js
+++ Screens/RegisterScreen.js
@@ -1,5 +1,12 @@
 import React, { useContext, useEffect } from "react";
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+  KeyboardAvoidingView,
+} from "react-native";
 import { register } from "../services/firebase";
 import { AuthContext } from "../components/AuthContext";
 
@@ -24,65 +31,70 @@ const Register = ({ navigation }) => {
   }, []);
 
   return (
-    <View className="flex-1 justify-center bg-white">
-      <View className="h-1/4">
-        <Text className="text-7xl pb-20 font-black text-center text-blue-500 pt-1">
+    <KeyboardAvoidingView
+      className='flex-1 justify-center bg-white'
+      behavior='padding'
+    >
+      <View className='h-1/4'>
+        <Text className='text-7xl pb-20 font-black text-center text-blue-500 pt-1'>
           Sign Up
         </Text>
       </View>
-      <View className="h-1/4">
+      <View className='h-1/4'>
         <TextInput
-          className="bg-gray-100 border border-gray-500 mx-10 p-4 rounded-lg"
-          title="Email"
-          placeholder="Email"
+          className='bg-gray-100 border border-gray-500 mx-10 p-4 rounded-lg'
+          title='Email'
+          placeholder='Email'
           blurOnSubmit={false}
           secureTextEntry={false}
-          keyboardType="email-address"
-          textContentType="emailAddress"
+          keyboardType='email-address'
+          textContentType='emailAddress'
           value={email}
           onChangeText={(value) => setEmail(value)}
-          returnKeyType="next"
+          onSubmitEditing={() => Keyboard.dismiss()}
+          returnKeyType='next'
         />
         <TextInput
-          className="bg-gray-100 border border-gray-500 mx-10 mt-5 p-4 rounded-lg"
-          title="Password"
-          placeholder="Password"
+          className='bg-gray-100 border border-gray-500 mx-10 mt-5 p-4 rounded-lg'
+          title='Password'
+          placeholder='Password'
           blurOnSubmit={false}
           secureTextEntry={true}
-          keyboardType="default"
-          textContentType="password"
+          keyboardType='default'
+          textContentType='password'
           value={password}
+          onSubmitEditing={() => Keyboard.dismiss()}
           onChangeText={(value) => setPassword(value)}
-          returnKeyType="next"
+          returnKeyType='next'
         />
         <TextInput
-          className="bg-gray-100 border border-gray-500 mx-10 mt-5 p-4 rounded-lg"
-          title="Confirm Password"
-          placeholder="Confirm Password"
+          className='bg-gray-100 border border-gray-500 mx-10 mt-5 p-4 rounded-lg'
+          title='Confirm Password'
+          placeholder='Confirm Password'
           blurOnSubmit={true}
           secureTextEntry={true}
-          keyboardType="default"
-          textContentType="password"
+          keyboardType='default'
+          textContentType='password'
           value={confirmPassword}
           onChangeText={(value) => setConfirmPassword(value)}
-          returnKeyType="done"
+          returnKeyType='done'
           onSubmitEditing={onRegisterPress}
         />
         <TouchableOpacity
-          className="bg-blue-500 mx-10 mt-5 py-5 rounded-lg"
+          className='bg-blue-500 mx-10 mt-5 py-5 rounded-lg'
           onPress={onRegisterPress}
         >
-          <Text className="text-xl text-center text-white">Create Account</Text>
+          <Text className='text-xl text-center text-white'>Create Account</Text>
         </TouchableOpacity>
         <Text
-          className="mt-5 text-center text-20 text-blue-900 text-sm"
+          className='mt-5 text-center text-20 text-blue-900 text-sm'
           onPress={onLoginPress}
           suppressHighlighting={true}
         >
           Already have an account?{"\n"}SIGN IN
         </Text>
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
