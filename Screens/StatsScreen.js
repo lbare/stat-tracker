6293010d033b9f9b31c468702e5e3cc94e557dd2
lbare <levi.bare@gmail.com>
2023-04-03 18:01:29 -0700

    testing with user login/signout

--- Screens/StatsScreen.js
+++ Screens/StatsScreen.js
@@ -1,8 +1,34 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useContext } from "react";
+import { TouchableOpacity } from "react-native";
+import { View, Text } from "react-native";
+import { AuthContext } from "../components/AuthContext";
+import { auth } from "../services/firebase";
 
 const Stats = () => {
-  return <></>;
+  const user = useContext(AuthContext);
+
+  const handleSignOut = async () => {
+    try {
+      console.log("Signing out...");
+      await auth.signOut();
+    } catch (error) {
+      console.error("Error signing out:", error);
+    }
+  };
+
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-3xl font-bold">
+        Hello, {user !== null ? user.email : "test"}
+      </Text>
+      <TouchableOpacity
+        className="bg-blue-500 rounded-lg p-4 mt-10"
+        onPress={handleSignOut}
+      >
+        <Text className="text-white text-2xl">Signout</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 export default Stats;
